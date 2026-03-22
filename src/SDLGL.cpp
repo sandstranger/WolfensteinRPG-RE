e70b4d22b4e85e0fb37fcfbf88402b60bb65e5be
sandstranger <sandstranger@gmail.com>
2026-03-22 11:26:36 +0400

    always use gles 3.1 for angle emulation

--- src/SDLGL.cpp
+++ src/SDLGL.cpp
@@ -123,10 +123,10 @@ bool SDLGL::Initialize() {
 
 #ifdef ANDROID
         SDL_Log(g_useGLES2_0 ? "Legacy OpenGL ES 2.0 is using for rendering" :
-                "OpenGL ES 3.2 is using for rendering");
+                "OpenGL ES 3.1 is using for rendering");
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, g_useGLES2_0 ? 2 : 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 2);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, g_useGLES2_0 ? 0 : 1);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_NO_ERROR, 1);
 #endif
 		//SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengles2");
